3fe9f0438cefa56e4a174d96a7b75bcf6372063b
AsharabzyanovaAR <rustamovna14082006@gmail.com>
2024-10-30 11:45:45 +0500

    Task4

---
+++ Tyuiu.AsharabzyanovaAR.Sprint3.Task4.V16.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Transactions;
+using Tyuiu.AsharabzyanovaAR.Sprint3.Task4.V16.Lib;
+namespace Tyuiu.AsharabzyanovaAR.Sprint3.Task4.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 2.757;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.AsharabzyanovaAR.Sprint3.Task4.V16/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.AsharabzyanovaAR.Sprint3.Task4.V16.Lib;
+using System.Linq.Expressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = ("Спринт #3 | Выполнила: Ашарабзянова А.Р. | ПКТб-24-1");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #16                                                             *");
+        Console.WriteLine("* Выполнила: Ашарабзянова Алина Рустамовна | ПКТб-24-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+        Console.WriteLine("* функции y=((cos(x)+x)/x)+0.25.При х = 0 пропустить значение Полученные  *");
+        Console.WriteLine("* значения перемножать.                                                   *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        
+        int startValue = -5;
+        int stopValue = 5;
+
+        
+        Console.WriteLine("Старт шага=" + startValue);
+        Console.WriteLine("Конец шага=" + stopValue);
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Произвдение ряда=" + ds.Calculate(startValue, stopValue));
+
+        Console.ReadKey();
+
+
+    }
+}
